b7d167f673ef6a9ae3401a7952232efcbed9221a
Aliaksandr Fiodarau <Aliaksandr_Fiodarau3@epam.com>
2023-04-03 14:48:01 +0300

    Add Collections Tasks (Custom ArrayList)

---
+++ javaCollections/src/main/java/org/course/collections/introduction/arraylist/CustomArrayList.java
@@ -0,0 +1,35 @@
+package org.course.collections.introduction.arraylist;
+
+/**
+ * Напишите минимальную реализацию класса ArrayList .
+ * <p>
+ * Требования:
+ * - Класс должен иметь возможность хранить объекты любого типа.
+ * - Класс должен иметь методы добавления элементов, получения элементов по индексу,
+ *   удаления элементов по индексу, получения размера списка.
+ * - Реализуйте динамическое изменение размера списка.
+ * - Реализуйте метод toString() для вывода содержимого списка в виде строки.
+ * - Напишите unit-tests для проверки работоспособности реализации.
+ */
+public class CustomArrayList {
+
+    public static void main(String[] args) {
+//        Пример вызова
+//        MyArrayList<String> list = new MyArrayList<>();
+//        list.add("Первый элемент");
+//        list.add("Второй элемент");
+//        list.add("Третий элемент");
+//        System.out.println(list); // Выводит: [Первый элемент, Второй элемент, Третий элемент]
+//
+//        list.remove(1);
+//        System.out.println(list); // Выводит: [Первый элемент, Третий элемент]
+//
+//        String secondElement = list.get(1);
+//        System.out.println(secondElement); // Выводит: Третий элемент
+//
+//        int size = list.size();
+//        System.out.println(size); // Выводит: 2
+
+    }
+
+}
